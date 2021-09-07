4b3f9ab12cd1880c2dd16ac836e5d4ec7eda8b02
buffelsafari <buffelsafari@hotmail.com>
2021-09-08 00:02:44 +0200

    some vies and modal

--- Garage3.Frontend/Controllers/Garages/GaragesController.cs
+++ Garage3.Frontend/Controllers/Garages/GaragesController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -50,5 +51,107 @@ namespace Garage3.Frontend.Controllers.Garages
 
             return View(modelList);
         }
+
+
+        public async Task<IActionResult> GarageOverview(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            GarageOverviewModelView model = new GarageOverviewModelView
+            {
+                GarageId=(int)id, 
+                GarageName="Name of the garage",
+                TableHead = new string[]{"PlateNumber", "Manufacturer", "VehicleType" },
+                Vehicles=new VehicleItemModelView[] 
+                {
+                    new VehicleItemModelView
+                    { 
+                        VehicleId=1,
+                        PlateNumber="ABC080",
+                        Manufacturer="Porsche",
+                        VehicleType="Car"
+                    },
+                    new VehicleItemModelView
+                    {
+                        VehicleId=2,
+                        PlateNumber="VIC0020",
+                        Manufacturer="Skoda",
+                        VehicleType="Car"
+                    },
+                    new VehicleItemModelView
+                    {
+                        VehicleId=3,
+                        PlateNumber="CMD064",
+                        Manufacturer="BMW",
+                        VehicleType="Car"
+                    },
+                    new VehicleItemModelView
+                    {
+                        VehicleId=4,
+                        PlateNumber="FYI076",
+                        Manufacturer="Honda",
+                        VehicleType="MotorCycle"
+                    }
+                }
+            };
+            
+
+            return View(model);
+        }
+
+
+        public async Task<IActionResult> Search(GarageOverviewModelView viewModel)
+        {
+
+            // todo search and sort
+            Debug.WriteLine("hello from filtered search");
+
+            GarageOverviewModelView model = new GarageOverviewModelView
+            {
+                GarageId = viewModel.GarageId,
+                GarageName=viewModel.GarageName,
+                TableHead = new string[] { "PlateNumber", "Manufacturer", "VehicleType" },
+                Vehicles = new VehicleItemModelView[]
+                {
+                    new VehicleItemModelView
+                    {
+                        VehicleId=1,
+                        PlateNumber="ABC080",
+                        Manufacturer="Porsche2",
+                        VehicleType="Car"
+                    },
+                    new VehicleItemModelView
+                    {
+                        VehicleId=2,
+                        PlateNumber="VIC0020",
+                        Manufacturer="Skoda2",
+                        VehicleType="Car"
+                    },
+                    new VehicleItemModelView
+                    {
+                        VehicleId=3,
+                        PlateNumber="CMD064",
+                        Manufacturer="BMW2",
+                        VehicleType="Car"
+                    },
+                    new VehicleItemModelView
+                    {
+                        VehicleId=4,
+                        PlateNumber="FYI076",
+                        Manufacturer="Honda2",
+                        VehicleType="MotorCycle"
+                    }
+                }
+            };
+
+
+
+            return View(nameof(GarageOverview), model);
+        }
+
+
     }
 }
---
+++ Garage3.Frontend/Models/ViewModels/GarageOverviewModelView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Garage3.Frontend.Models.ViewModels
+{
+    public class GarageOverviewModelView
+    {
+        public int GarageId { get; set; }
+
+        public string GarageName { get; set; }
+        
+        public IEnumerable<string> TableHead { get; set; }
+        public IEnumerable<VehicleItemModelView> Vehicles { get; set; }
+
+    }
+}
