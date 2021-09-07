04a26686cf95b5a87370d20c53085a44ae24893a
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-09-07 16:44:48 +0200

    appsettings och depndency injection EF

--- Garage3.Frontend/Startup.cs
+++ Garage3.Frontend/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage3.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Garage3.Frontend
 {
@@ -24,6 +26,9 @@ namespace Garage3.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<GarageContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("GarageContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
