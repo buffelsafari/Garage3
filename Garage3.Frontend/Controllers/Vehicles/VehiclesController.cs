d55a8e4e9449f7bf6707ab556f71fa592040efd8
buffelsafari <buffelsafari@hotmail.com>
2021-09-08 22:26:27 +0200

    a fake vehicle service, and some script bugs

--- Garage3.Frontend/Controllers/Vehicles/VehiclesController.cs
+++ Garage3.Frontend/Controllers/Vehicles/VehiclesController.cs
@@ -1,4 +1,6 @@
-﻿using Garage3.Frontend.Models.ViewModels;
+﻿using Garage3.Data.Entities;
+using Garage3.Frontend.Models.ViewModels;
+using Garage3.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,46 +12,23 @@ namespace Garage3.Frontend.Controllers.Vehicles
 {
     public class VehiclesController:Controller
     {
+        private IVehicleService vehicleService;
+        public VehiclesController(IVehicleService vehicleService)
+        {
+            this.vehicleService = vehicleService;
+        }
 
         public async Task<IActionResult> Index()
         {
-            
+            IEnumerable<Vehicle> vehicles = await vehicleService.FindVehicles(new FindVehicleArgs { });
+
 
             VehicleOverviewModelView model = new VehicleOverviewModelView
             {
-                
+
+
                 TableHead = new string[] { "PlateNumber", "Manufacturer", "VehicleType" },
-                Vehicles = new VehicleItemModelView[]
-                {
-                    new VehicleItemModelView
-                    {
-                        VehicleId=1,
-                        PlateNumber="ABC080",
-                        Manufacturer="Porsche",
-                        VehicleType="Car"
-                    },
-                    new VehicleItemModelView
-                    {
-                        VehicleId=2,
-                        PlateNumber="VIC0020",
-                        Manufacturer="Skoda",
-                        VehicleType="Car"
-                    },
-                    new VehicleItemModelView
-                    {
-                        VehicleId=3,
-                        PlateNumber="CMD064",
-                        Manufacturer="BMW",
-                        VehicleType="Car"
-                    },
-                    new VehicleItemModelView
-                    {
-                        VehicleId=4,
-                        PlateNumber="FYI076",
-                        Manufacturer="Honda",
-                        VehicleType="MotorCycle"
-                    }
-                }
+                Vehicles = vehicles.Select(v => new VehicleItemModelView{ VehicleId = v.Id, PlateNumber=v.PlateNumber, Manufacturer=v.Manufacturer, VehicleType=v.VehicleType.Name })
             };
 
 
@@ -59,6 +38,9 @@ namespace Garage3.Frontend.Controllers.Vehicles
 
         public async Task<IActionResult> Search(VehicleOverviewModelView viewModel)
         {
+
+
+            
 
             // todo search and sort
             Debug.WriteLine("hello from filtered search");
