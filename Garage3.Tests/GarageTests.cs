f96c7efe7511a16f0ab81a93874fb6d1c81cd737
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-09-08 10:06:52 +0200

    testproject

---
+++ Garage3.Tests/GarageTests.cs
@@ -0,0 +1,37 @@
+using System.IO;
+using System.Reflection.Metadata;
+using System.Reflection;
+using NUnit.Framework;
+using System.Linq;
+using System;
+using Garage3.Data;
+using Garage3.Tests.Utilities;
+
+namespace Garage3.Tests
+{
+    [TestFixture]
+    public abstract class GarageTests : SetupFixture
+    {
+        protected override Database<GarageContext> ContextManager { get; set; }
+    }
+
+    [TestFixture]
+    [Category("InMemory")]
+    public class GarageInMemoryTests : GarageTests
+    {
+        protected override void Configure()
+        {
+            ContextManager = new SqliteInMemoryDb<GarageContext>();
+        }
+    }
+
+    [TestFixture]
+    [Category("LocalDB")]
+    public class GarageLocalDBTests : GarageTests
+    {
+        protected override void Configure()
+        {
+            ContextManager = new LocalDb<GarageContext>();
+        }
+    }
+}
---
+++ Garage3.Tests/Utilities/SetupFixture.cs
@@ -0,0 +1,54 @@
+﻿using Garage3.Data;
+using NUnit.Framework;
+
+namespace Garage3.Tests.Utilities
+{
+    [TestFixture]
+    public abstract class SetupFixture
+    {
+        protected abstract Database<GarageContext> ContextManager { get; set; }
+
+        protected abstract void Configure();
+
+        /// <summary>
+        /// Runs (ones) before all testcases executes
+        /// </summary>
+        [OneTimeSetUp]
+        public virtual void OneTimeSetup()
+        {
+            // Configure db provider to either use sqlite(inmemory) or localdb
+            // and set ContextManager
+            Configure();
+
+            ContextManager.CreateDB();
+        }
+
+        /// <summary>
+        /// Runs (ones) after all testcases have finished executing 
+        /// </summary>
+        [OneTimeTearDown]
+        public virtual void OneTimeTearDown() 
+        {
+            // Cleanup DB
+            ContextManager.Dispose();
+        }
+
+        /// <summary>
+        /// Runs before each testcase
+        /// </summary>
+        [SetUp]
+        public virtual void Setup()
+        {
+
+        }
+
+        /// <summary>
+        /// Runs after each test case
+        /// </summary>
+        [TearDown]
+        public virtual void TearDown()
+        {
+
+        }
+    }
+}
