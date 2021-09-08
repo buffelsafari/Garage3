69d25fc929c0b91ac653b788f0bcf7fcaa4a7eb9
Artem <46307494+artorb@users.noreply.github.com>
2021-09-08 13:56:27 +0300

    Validation

---
+++ Garage3.Data/Exceptions/GarageException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Garage3.Data.Exceptions
+{
+    public class GarageException : Exception
+    {
+        public GarageException()
+        {
+        }
+
+        public GarageException(string message) : base(message)
+        {
+        }
+    }
+    
+    public class MemberExistsInDbException : GarageException
+    {
+        
+    }
+
+    public class NotEmptyException : GarageException
+    {
+        public NotEmptyException(string message) : base(message + " field is empty")
+        {
+        }
+    }
+    
+    public class InvalidPersonalNumberException : GarageException
+    {
+        public string InvalidPersonalNumber { get; }
+
+        public InvalidPersonalNumberException(string number) : base(number + " is invalid")
+        {
+            InvalidPersonalNumber = number;
+        }
+    }
+}
---
+++ Garage3.Data/Validation/MemberValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using Garage3.Data.Entities;
+
+namespace Garage3.Data.Validation
+{
+    public class MemberValidator : AbstractValidator<Member>
+    {
+        public MemberValidator()
+        {
+            RuleFor(mem => mem.FirstName).NotEmpty().WithMessage("Firstname cannot be empty"); // nåt annat här? regex?
+            RuleFor(mem => mem.Surname).NotEmpty().WithMessage("Lastname cannot be empty"); // nåt annat här? regex?
+            RuleFor(mem => mem.PhoneNumber).Matches(@"[0-9]").NotEmpty().WithMessage("Phone number cannot be empty");
+            RuleFor(mem => mem.PersonalNumber).Matches(@"[]");
+        }
+    }
+
+    public class VehicleValidator : AbstractValidator<Vehicle>
+    {
+        public VehicleValidator()
+        {
+            RuleFor(veh => veh.Model).NotEmpty().WithMessage("Model cannot be empty");
+            RuleFor(veh => veh.Manufacturer).NotEmpty().WithMessage("Manufacturer cannot be empty");
+            RuleFor(veh => veh.Wheels).Must(c => c is <= 6 and >= 2).WithMessage("Cannot be less than 2 or more than 6 wheels"); // 2 ? eller 4
+            RuleFor(veh => veh.PlateNumber).Matches(@"[]"); // regex 0-6
+        }
+    }
+}
