3a39db0b8c9724216254c76f34c2fc20b13622c0
Artem <46307494+artorb@users.noreply.github.com>
2021-09-08 09:31:45 +0300

    Small Service

---
+++ Garage3.Services/IBookingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Garage3.Data.Entities;
+
+namespace Garage3.Services
+{
+    public interface IBookingService
+    {
+        Result<Booking> CheckinVehicle(MembershipType membershipType, string plateNumber, DateTime timeOfArrival,
+            IEnumerable<string> parkingLotNumbers);
+
+        Result<Receipt> CheckoutVehicle(string plateNumber);
+    }
+}
---
+++ Garage3.Services/IMemberService.cs
@@ -0,0 +1,11 @@
+﻿using Garage3.Data.Entities;
+
+namespace Garage3.Services
+{
+    public interface IMemberService
+    {
+        Member RegisterMember(string plateNumber, string personalNumber, string firstName, string lastName, string phoneNumber);
+
+        Member GetMember(string personalNumber);
+    }
+}
