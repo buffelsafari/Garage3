fafde9bb94149a5aaf06035ab0cf6990e1cb348b
sandelinvs@outlook.com <sandelinvs@outlook.com>
2021-09-08 16:34:23 +0200

    refaktorerat interfacen

---
+++ Garage3.Services/IMemberService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Garage3.Data.Entities;
+
+namespace Garage3.Services
+{
+    public interface IMemberService
+    {
+        Task<Member> RegisterMember(RegisterMemberArgs args, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<Member>> FindMembers(string personalNumber, CancellationToken cancellationToken = default);
+    }
+
+    public class RegisterMemberArgs
+    {
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string PersonalNumber { get; set; }
+
+        public virtual MembershipType MembershipType { get; set; }
+    }
+}
---
+++ Garage3.Services/IMembershipTypeService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Garage3.Data.Entities;
+
+namespace Garage3.Services
+{
+    public interface IMembershipTypeService
+    {
+        Task<MembershipType> CreateNewMembershipType(NewMemberShipTypeArgs args, CancellationToken cancellationToken = default);
+    }
+
+    public class NewMemberShipTypeArgs
+    {
+        public string Name { get; set; }
+
+        public int GarageId { get; set; }
+    }
+}
